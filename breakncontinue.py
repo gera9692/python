92340541da6628abe6c7be2ccecbced41137041a
gerald manyasa <93513811+gera9692@users.noreply.github.com>
2022-12-21 16:57:14 +0300

    Create breakncontinue.py

---
+++ breakncontinue.py
@@ -0,0 +1,13 @@
+#this is for the break and continue statement
+print("inside a breaking loop")
+for br in range(40,100):
+ if br == 60:
+     break
+ print("this is:",br)
+ 
+print("this is the continue statement")
+for br in range(1,100):
+ if br ==60:
+     continue
+ print("here is:",br)
+ 
